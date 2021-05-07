fa381d5a3490f911710a2b28f747363c189e5163
jsebas2220 <jsebas22@hotmail.com>
2021-05-06 20:38:23 -0500

    LaboratorioAWS

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> DATOS </title>
+</head>
+<body>
+
+    <?php
+
+    $nombre = "JOHAN SEBASATIAN JIMENEZ MOLINA";
+    echo "Mi Nombre es " . $nombre;
+    echo "<br>";
+    echo "Codigo : 220062175";
+
+    ?>
+
+</body>
+</html>
